c0af41608c38922c8ab22369f6b962c2549940b7
Andrew Polaskey <apolaskey@gmail.com>
2018-08-09 02:31:30 -0400

    Revert "Initial Commit"
    This reverts commit 5b4c0a02656c236809ae92220666ea4935dfb6e3.

--- src/app/index.ts
+++
@@ -1,4 +0,0 @@
-import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
-import {IEXAppModule} from "./modules/IEXAppModule";
-
-platformBrowserDynamic().bootstrapModule(IEXAppModule);
